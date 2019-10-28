3253cd9f903129cb55bd0b18128bbbb73aec92ec
Christoph Vitz <c.vitz@itsonix.eu>
2019-10-28 12:56:40 +0100

    PA-2281: MOve `status` to beginning of message
    This makes it easier to see the status in sentry ui.

--- src/customFingerprintHandlers.js
+++ src/customFingerprintHandlers.js
@@ -4,7 +4,7 @@ const axiosError = {
   matches: error => !!error.request && !!error.response && !!error.config,
   handleError: error => ![401, 403].includes(error.response.status),
   getFingerprint: error => ['{{ default }}', error.config.method, replaceUUIDs(error.config.url), error.response.status],
-  getMessage: error => `${error.config.method} ${error.config.url}: ${error.response.status}`,
+  getMessage: error => `${error.response.status}: ${error.config.method} ${error.config.url}`,
 }
 
 export default [
