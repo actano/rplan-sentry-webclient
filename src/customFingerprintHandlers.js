c62ea4053d63fc933358228765b232353d07cd6b
thrucker <t.rucker@xpure.de>
2020-01-06 12:49:33 +0100

    PA-2812: Add detail text to sentry event

--- src/customFingerprintHandlers.js
+++ src/customFingerprintHandlers.js
@@ -1,10 +1,25 @@
+import get from 'lodash/get'
+
 const replaceUUIDs = string => string.replace(/[a-f0-9-]{36}/, ':id')
+
+const getStatusDetail = (error) => {
+  const contentType = get(error, ['response', 'headers', 'content-type'])
+
+  if (contentType === 'text/plain') {
+    return error.response.data
+  }
+
+  return ''
+}
 
 const axiosError = {
   matches: error => !!error.request && !!error.response && !!error.config,
-  handleError: error => ![401, 403].includes(error.response.status),
-  getFingerprint: error => ['{{ default }}', error.config.method, replaceUUIDs(error.config.url), error.response.status],
-  getMessage: error => `${error.response.status}: ${error.config.method} ${error.config.url}`,
+  handleError: error => ![401, 404].includes(error.response.status),
+  getFingerprint: error => ['{{ default }}', error.config.method, replaceUUIDs(error.config.url), error.response.status, getStatusDetail(error)],
+  getMessage(error) {
+    const statusText = `${error.response.status} ${getStatusDetail(error)}`.trim()
+    return `${statusText}: ${error.config.method} ${error.config.url}`
+  },
 }
 
 export default [
