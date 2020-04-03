860bfc6f574446cd467c57c562f2320eca4e479e
Christoph Vitz <c.vitz@itsonix.eu>
2020-04-03 08:36:55 +0200

    PA-4155: Only use first matching handler
    If multiple handlers were to match, only the first one would modify the error event.

--- src/index.js
+++ src/index.js
@@ -18,19 +18,19 @@ const initClient = () => {
     beforeSend(event, hint) {
       const error = hint.originalException
 
-      // see: https://docs.sentry.io/data-management/event-grouping/sdk-fingerprinting/?platform=javascript
-      for (const handler of fingerprintHandlers) {
-        if (handler.matches(error)) {
-          if (!handler.handleError(error)) {
-            return undefined
-          }
-          const fingerprint = handler.getFingerprint(error)
-          const message = handler.getMessage(error)
+      const handler = fingerprintHandlers.find(h => h.matches(error))
 
-          set(event, 'fingerprint', fingerprint)
-          if (message) {
-            set(event, 'exception.values[0].value', message)
-          }
+      // see: https://docs.sentry.io/data-management/event-grouping/sdk-fingerprinting/?platform=javascript
+      if (handler) {
+        if (!handler.handleError(error)) {
+          return undefined
+        }
+        const fingerprint = handler.getFingerprint(error)
+        const message = handler.getMessage(error)
+
+        set(event, 'fingerprint', fingerprint)
+        if (message) {
+          set(event, 'exception.values[0].value', message)
         }
       }
 
